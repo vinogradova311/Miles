49b282cfedf481489fdee56787177f5c24d6f52c
vinogradova311 <anny.amelie@gmail.com>
2022-07-09 17:56:22 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 50;
+        int rublesForMile = 20 ;
+
+        int bonusMiles = amount / rublesForMile;
+        System.out.println(bonusMiles);
+    }
+}
